b904860ed74ce5024fb9d3e5fb48fad89344cd53
Danny Nguyen <nguyenduongpa@gmail.com>
2023-07-09 14:33:03 -0700

    fixed up responsiveness

--- client/src/components/TopNavigation.js
+++ client/src/components/TopNavigation.js
@@ -26,7 +26,11 @@ const TopNavigation = (props) => {
             <div className={Css.locationDivSmall}>
                 {
                     user.location?
-                    <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/><h6 className={Css.locationFont}>{user.location}</h6></Link>
+                    <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/>
+                        <div>
+                            <h6 className={Css.locationFont}>{user.location}</h6>
+                        </div>
+                    </Link>
                     : <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/><h6 className={Css.locationFont}>None set yet..</h6></Link>
 
                 }
@@ -43,8 +47,12 @@ const TopNavigation = (props) => {
                 <div className={Css.locationDiv}>
                     {
                         user.location?
-                        <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/><h6 className={Css.locationFont}>{user.location}</h6></Link>
-                        : <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/><h6 className={Css.locationFont}>None set yet..</h6></Link>
+                        <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/>
+                            <div className={Css.locationText}>
+                                <h6 className={Css.locationFont}>{user.location}</h6>
+                            </div>
+                        </Link>
+                        : <Link to='/users/edit'><img src={ping} className={Css.pingIcon}/><h6 className={Css.locationFont}>No location set yet..</h6></Link>
 
                     }
                 </div>
